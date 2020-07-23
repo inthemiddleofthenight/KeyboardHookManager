0262f30ea4f7871741aaef598cad39707f406a60
Elijah_M <sterva.konserva@yandex.ru>
2020-07-23 09:04:51 +0300

    preview

---
+++ KeyboardHookManager/KeyboardHookManager.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace KeyboardHookManager.TestApp
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            HookManager.KeyDown += HookManager_KeyDown;
+            HookManager.KeyPress += HookManager_KeyPress;
+            HookManager.KeyUp += HookManager_KeyUp;
+        }
+
+        private void HookManager_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            KeyboardLog.Text += $"KeyUp {e.KeyCode.ToString()}\n";
+        }
+
+        private void HookManager_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            KeyboardLog.Text += $"KeyPress {e.KeyChar}\n";
+        }
+
+        private void HookManager_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            KeyboardLog.Text += $"KeyDown {e.KeyCode.ToString()}\n";
+        }
+    }
+}
